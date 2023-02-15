dfe05eeee35907cfabc74c1ea18a285dcdf3213a
marina <marina@gmail.com>
2023-02-15 19:24:15 +0300

    stage03

---
+++ lesson9/task_02.py
@@ -0,0 +1,12 @@
+def check_player(age, sex):
+    return 15 <= age <= 18 and sex == "f"
+
+
+def main():
+    age = int(input("Input your age: "))
+    sex = input("Input your sex (m/f): ")
+    msg = "YES" if check_player(age, sex) else "NO"
+    print(msg)
+
+
+main()
---
+++ lesson9/task_03.py
@@ -0,0 +1,22 @@
+def call_age(age):
+    if not (0 <= age <= 150):
+        return "Error. User data was invalid."
+
+    if age <= 13:
+        msg = "Childhood"
+    elif age <= 34:
+        msg = "Youth"
+    elif age <= 59:
+        msg = "Maturity"
+    else:
+        "Old age"
+
+    return msg
+
+
+def main():
+    age = int(input("Input your age: "))
+    print(call_age(age))
+
+
+main()

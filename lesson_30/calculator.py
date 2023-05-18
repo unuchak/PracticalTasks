6ebee5ab2eab302169781ebd6d3b2829146f64d6
marina <marina.unuchak@gmail.com>
2023-05-18 20:53:48 +0300

    unittest calculator_test.py

---
+++ lesson_30/calculator.py
@@ -0,0 +1,14 @@
+class Calculator:
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+    @staticmethod
+    def sub(a, b):
+        return a - b
+    @staticmethod
+    def mul(a, b):
+        return a * b
+    @staticmethod
+    def div(a, b):
+        return a / b
